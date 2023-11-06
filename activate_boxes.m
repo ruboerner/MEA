76a8fd6200999df19ece9dc1c767131cc0851f57
Ralph-Uwe Börner <rub@geophysik.tu-freiberg.de>
2023-11-06 08:46:32 +0100

    Fix pole-dipole error

--- activate_boxes.m
+++ activate_boxes.m
@@ -26,36 +26,42 @@ assert(a ~= b && a ~= m && a ~= n);
 assert(b ~= m && b ~= n);
 assert(m ~= n);
 
+if all(boxes)
+    write(port, [0, a, b, m, n], 'uint8');
+    while port.NumBytesAvailable < 8
+        pause(0.01);
+    end
+    ok = read(port, port.NumBytesAvailable, 'uint8');
+    % The array `ok` is an 8x1 array of uint8.
+    % It contains 4 pairs of address and return value for the electrodes A, B, M,
+    % and N
+    % First, check that the addresses are copied correctly:
+    %
+    ok1 = isequal([a, b, m, n], ok(1:2:8));
+    % Next, check the return codes.
+    % If A, B, M, N are `on` the four bytes return
+    % 136, 72, 40 and 24, respectively
+    % Expressed in binary format:
+    % ABMNx000, where bit x contains 1 for `on` or 0 for `off`
+    %
+    % For pole-dipole configurations it is assumed that electrode B
+    % is never activated in any box.
+    % In that case we expect that the state of the relais for b is OFF.
+    %
+    if b > 0 % 4 electrodes
+        ok2 = isequal([136, 72, 40, 24], ok(2:2:8));
+    else % 3 electrodes
+        ok2 = isequal([136, 64, 40, 24], ok(2:2:8));
+    end
 
-write(port, [0, a, b, m, n], 'uint8');
-
-while port.NumBytesAvailable < 8
-    pause(0.01);
-end
-
-ok = read(port, port.NumBytesAvailable, 'uint8');
-
-% The array `ok` is an 8x1 array of uint8.
-% It contains 4 pairs of address and return value for the electrodes A, B, M,
-% and N
-% First, check that the addresses are copied correctly:
-%
-ok1 = isequal([a, b, m, n], ok(1:2:8));
-
-% Next, check the return codes.
-% If A, B, M, N are `on` the four bytes return
-% 136, 72, 40 and 24, respectively
-% Expressed in binary format:
-% ABMNx000, where bit x contains 1 for `on` or 0 for `off`
-%
-% For pole-dipole configurations it is assumed that electrode B
-% is never activated in any box.
-% In that case we expect that the state of the relais for b is OFF.
-%
-if b > 0 % 4 electrodes
-    ok2 = isequal([136, 72, 40, 24], ok(2:2:8));
-else % 3 electrodes
-    ok2 = isequal([136, 64, 40, 24], ok(2:2:8));
+elseif isequal(b, 0)
+    write(port, [0, a, m, n], 'uint8');
+    while port.NumBytesAvailable < 6
+        pause(0.01);
+    end
+    ok = read(port, port.NumBytesAvailable, 'uint8');
+    ok1 = isequal([a, m, n], ok(1:2:6));
+    ok2 = isequal([136, 40, 24], ok(2:2:6));
 end
 
 % Return true if both checks are ok.
